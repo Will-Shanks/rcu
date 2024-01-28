05e95daeca5f6e0f8b32fcfbfb2b617f0558aaa0
Will Shanks <git@wshanks.com>
2024-01-28 15:13:24 -0800

    Implement RCU traits
    Impl traits RCU, RcuHandle, and RcuGuard to be used to make code generic
    over its RCU implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,24 @@
 #![deny(unsafe_op_in_unsafe_fn)]
-
 pub mod cds;
 pub mod qsbr;
 pub mod utils;
+
+pub trait RCU {
+    type Handle<'a>: RcuHandle<'a>
+    where
+        Self: 'a;
+    fn new() -> Self;
+    fn register(&self, id: u64) -> Self::Handle<'_>;
+}
+
+pub trait RcuHandle<'a> {
+    type Guard<'g>: RcuGuard<'g>
+    where
+        'a: 'g;
+    fn read(&self) -> Self::Guard<'a>;
+    fn quiescent_state(&mut self);
+    fn sync(&self);
+    fn quiescent_sync(&mut self);
+}
+
+pub trait RcuGuard<'a> {}
